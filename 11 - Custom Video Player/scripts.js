bcd479f33db8a1ae653cc3c77c2bc7e9ef4d1c76
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-01-28 18:32:27 +0900

    added play and pause icon

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -16,8 +16,13 @@ function togglePlay() {
   }
 }
 
-function updateButton() {}
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
 
 //Hook up the event listeners
 video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
 toggle.addEventListener("click", togglePlay);
