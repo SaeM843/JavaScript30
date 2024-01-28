f7a3393add5ed5f20f230732719536db29381d3c
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-01-28 18:25:48 +0900

    added methods to play the video

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,23 @@
+//Get our elements
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+//Build out functions
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {}
+
+//Hook up the event listeners
+video.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
