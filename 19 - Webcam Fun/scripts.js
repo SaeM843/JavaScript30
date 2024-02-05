63eb473e109784bdae5df397ec14d1a259c2e05a
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-02-05 15:58:41 +0900

    completed a takePhoto function

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -33,6 +33,17 @@ function paintToCanvas() {
 function takePhoto() {
   snap.currentTime = 0;
   snap.play();
+
+  // take the data out of canvas
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "Pretty");
+  link.innerHTML = `<img src=${data} alt="cute face">`;
+  console.log(data);
+  strip.insertBefore(link, strip.firstChild);
 }
+
+video.addEventListener("canplay", paintToCanvas);
 
 getVideo();
