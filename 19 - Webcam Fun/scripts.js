0a4788a627d19570625257b7b3e278d9c388d940
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-02-05 16:21:01 +0900

    added a rgb split function

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -25,15 +25,17 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-
-    //take the pixles out
+    // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
-    //mess with them
-    pixels = redEffect(pixels);
+    // mess with them
+    // pixels = redEffect(pixels);
 
-    //put them back
-    ctx.putImageData(pixels, width, height);
-    //console.log(pixels);
+    pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+
+    // pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -58,6 +60,15 @@ function redEffect(pixels) {
     pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
     pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
   }
   return pixels;
 }
