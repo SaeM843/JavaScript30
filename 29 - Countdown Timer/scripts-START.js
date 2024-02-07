285f96e10fa9efcdb05b17e394cc422bf4c5d0b2
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-02-07 14:56:02 +0900

    added a formula to calcurate mins and seconds

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -17,5 +17,7 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
-  console.log(seconds);
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  console.log({ minutes, remainderSeconds });
 }
