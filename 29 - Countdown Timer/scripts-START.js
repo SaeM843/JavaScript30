3bfa4b7955737956bb519dc605ab9e69b005531d
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-02-07 15:23:04 +0900

    displayEndTime function working

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,11 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   //console.log({ now, then });
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -26,4 +28,12 @@ function displayTimeLeft(seconds) {
   }${remainderSeconds}`;
   timerDisplay.textContent = display;
   document.title = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back at ${hour}:${minutes}`;
+  console.log(end);
 }
